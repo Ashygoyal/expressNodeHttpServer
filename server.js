887f5fe5ba894b3262779bb767009912bd9bac0c
Ashish Goyal <ashish@moonraft.com>
2018-05-22 14:42:38 +0530

    Testing Get Calls

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('<b> My first Express HTTP Server');
+});
+
+app.get('/Welcome', function(req, res){
+    res.send('<b> Welcome </b> to my HTTP server!!');
+});
+
+app.use(function(req, res, next){
+    res.status(404).send("Sorry, that route doesnt exist!");
+});
+
+app.listen(3000, function(){
+    console.log('Your app is now running on port 3000');
+});
