2a3f29cc10b8a169fc7017d45e1dce85d883c37d
Ashish Goyal <ashish@moonraft.com>
2018-05-25 15:56:33 +0530

    adding res.send in POST request

--- postRequest.js
+++ postRequest.js
@@ -19,6 +19,7 @@ app.post('/login',function(req,res){
   var user_name=req.body.user;
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
+  res.send('<b>Congrats</b>');
   res.end("yes");
 });
 
